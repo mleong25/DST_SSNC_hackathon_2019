555483cedd61ccce3b1d27f03f6d14d19caa977c
Marcus Leong <MLEONG@dstsystems.com>
2019-07-22 12:55:00 -0500

    create ui branch

---
+++ ui/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CcbComponent } from './ccb/ccb.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CcbComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
