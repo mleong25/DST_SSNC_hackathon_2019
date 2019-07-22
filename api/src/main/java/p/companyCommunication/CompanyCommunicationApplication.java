1e8764408b337a8a1b81c12f82afae11e9dcc47c
Marcus Leong <MLEONG@dstsystems.com>
2019-07-22 12:15:15 -0500

    project restructure

---
+++ api/src/main/java/p/companyCommunication/CompanyCommunicationApplication.java
@@ -0,0 +1,13 @@
+package p.companyCommunication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyCommunicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyCommunicationApplication.class, args);
+	}
+
+}
