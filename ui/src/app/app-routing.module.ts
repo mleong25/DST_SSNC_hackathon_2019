244c731018dcd7a872d418d63c641e3e18c9e391
Samantha Kreisel <samikreisel@gmail.com>
2019-08-01 14:09:38 -0500

    added menu and angular material

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CcbComponent} from './ccb/ccb.component';
+import { AboutUsComponent} from './about-us/about-us.component';
 import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
   { path: 'ccb', component: CcbComponent },
+  { path: 'about-the-team', component: AboutUsComponent },
   { path: '', component: CcbComponent}
 ];
 
--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -4,15 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CcbComponent } from './ccb/ccb.component';
-
+import { MenuComponent } from './menu/menu.component';
+import { MatIconModule,
+  MatToolbarModule,
+  MatButtonModule
+} from '@angular/material';
+import { AboutUsComponent } from './about-us/about-us.component';
 @NgModule({
   declarations: [
     AppComponent,
-    CcbComponent
+    CcbComponent,
+    MenuComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
